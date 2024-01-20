5b715aa6f8be55cafacf045c64b6871326bef873
Nazarii Havryliuk <ipz234_gnp@student.ztu.edu.ua>
2024-01-20 12:34:38 +0200

    Створив клас Laptop

---
+++ ClassLibrary/Laptop.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Laptop : Product
+    {
+        public string Processor {  get; set; }
+        public string Memory { get; set; }
+
+        public Laptop(string processor, string memory, string name, double price, int count)
+            : base(name, price, count) 
+        {
+            Processor = processor; 
+            Memory = memory; 
+        }
+        public override string Display()
+        {
+            return ($"{Name} {Price} {Count} {Processor} {Memory}");
+            throw new NotImplementedException();
+        }
+    }
+}
