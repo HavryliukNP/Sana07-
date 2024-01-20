77b5c113eabd98b19d5b3f52421ccedb888435a3
Nazarii Havryliuk <ipz234_gnp@student.ztu.edu.ua>
2024-01-20 12:47:00 +0200

    Створив клас Lego

---
+++ ClassLibrary/Lego.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Lego : Product
+    {
+        public int NumberParts { get; set; }
+
+        public Lego(int numberParts, string name, double price, int count)
+            : base(name, price, count)
+        {
+            NumberParts = numberParts;
+        }
+        public override string Display()
+        {
+            return ($"{Name} {Price} {Count} {NumberParts}");
+            throw new NotImplementedException();
+        }
+    }
+}
